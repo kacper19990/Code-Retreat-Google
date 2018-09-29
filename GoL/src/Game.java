3f8209c79dddd8e995f9392b3940c66524bd8050
Kacper <kacper199902@gmail.com>
2018-09-29 15:51:24 +0100

    Run 3

--- GoL/src/Game.java
+++ GoL/src/Game.java
@@ -3,6 +3,14 @@ public class Game
 
     int[][] board;
     public Game(int x, int y) {
-        board = new int[x][y];
+        this.board = new int[x][y];
+    }
+
+    public int getCell(int x, int y){
+        return this.board[x][y];
+    }
+
+    public void setCell(int value, int x, int y) {
+        this.board[x][y] = value;
     }
 }
--- GoL/src/GameTest.java
+++ GoL/src/GameTest.java
@@ -1,6 +1,10 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class GameTest {
+
+    @Test
     public void testSet () {
         int x = 10;
         int y = 10;
@@ -8,6 +12,16 @@ public class GameTest {
         int expected = x;
 
         assertEquals(expected, g.board.length);
+    }
+
+    @Test
+    public void testActualSet () {
+        int x = 10;
+        int y = 10;
+        Game g = new Game(x, y);
+
+        g.setCell(5, 5, 1);
+        assertEquals(1, g.getCell(5, 5));
     }
 
 }
